8e2836ab83a472de5885bb42fb2ea364d16c92d8
aaron <aaron@AARON-PC>
2023-03-04 16:42:49 +0000

    Add project files.

---
+++ MVC_Reflection/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Reflection.Models;
+
+namespace MVC_Reflection.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            using (DBEntities6 db = new DBEntities6())
+            {
+                List<Employee> employees = db.Employees.ToList();
+                List<Company> companies = db.Companies.ToList();
+
+                var result = from emp in employees
+                             join comp in companies on emp.DeptId equals comp.DeptId into table1
+                             select new ViewModel
+                             {
+                                 employee = emp,
+                             };
+
+                return View(result);
+            }
+        }
+    }
+}
---
+++ MVC_Reflection/Models/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Reflection.Models
+{
+    public class ViewModel
+    {
+
+        public Employee employee
+        {
+            get; set;
+        }
+
+        public Company company
+        {
+            get; set;
+        }
+    }
+}
